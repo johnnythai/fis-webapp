6c9cf188e6090c9d69ac501b5f25e301ae89421e
Johnny Thai <jthai04@gmail.com>
2023-09-12 15:01:07 -0700

    forms

--- src/components/forms/customerRelationshipSummaryForm.tsx
+++ src/components/forms/customerRelationshipSummaryForm.tsx
@@ -1,5 +1,5 @@
-import {type} from "os";
 import { useRef } from "react";
+import {fetchApi} from "../../api/fetchApi";
 
 const CustomerRelationshipSummaryFields = (props: any) => {
 
@@ -11,23 +11,58 @@ const CustomerRelationshipSummaryFields = (props: any) => {
 			</label>
 			<br />
 			<label>
-				Account Number: <input name="tin" type="number" maxLength={20} ref={props.accountNumberRef}/>
+				Account Number: <input name="tin" pattern="[0-9]*"  maxLength={20} ref={props.accountNumberRef}/>
 			</label>
 		
 		</>
 	);
 };
 
-const CustomerRelationshipSummaryForm = () => {
+const CustomerRelationshipSummaryForm = (props: {horizonToken: string}) => {
 	const customerIdRef = useRef<HTMLInputElement>();
 	const accountNumberRef = useRef<HTMLInputElement>();
 
-	const handleSubmit = (e: any) => {
+	const fetchCustomerId = () => {
+		const applicationCode = 'DD';
+		const accountNumber = accountNumberRef.current.value;
+	
+		const fetchOptions = {
+			headers: {
+				horizonToken: props.horizonToken,	
+			},
+		};
+
+		const customerId = fetchApi(`/api/horizon/${applicationCode}/${accountNumber}`, fetchOptions);
+		return customerId;
+	};
+	
+	const fetchCustomerRelationshipSummary = async (formData: FormData) => {
+		if (!customerIdRef.current?.value) {
+			const customerId = await fetchCustomerId();
+		} else {
+			const customerId = customerIdRef.current.value;
+		}
+
+		const fetchOptions = {
+			headers: {
+				horizonToken: props.horizonToken,
+			},
+			body: JSON.stringify(formData)
+		};
+		
+		const customerRelationshipSummary = await fetchApi(`/api/horizon/${customerId}/relationship-summary`, fetchOptions)
+		console.log('CUSTOMER RELATIONSHIP SUMMARY: ', customerRelationshipSummary);
+	};
+
+	const handleSubmit = async (e: any) => {
 		e.preventDefault();
-		const formData = new FormData(e.target);
+
 		if (!accountNumberRef.current?.value && !customerIdRef.current?.value) {
 			alert('At least one field is required.');
 		} 
+
+		const formData = new FormData(e.target);
+		await fetchCustomerRelationshipSummary(formData);
 	};
 
 	return(
