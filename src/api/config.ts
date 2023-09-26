f3723c414fc061c2dd16ec152f9ad931c1273dfa
Johnny Thai <jthai04@gmail.com>
2023-09-25 17:42:46 -0700

    config

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
-// const apiUrl = 'https://fis.johnnythai.dev'
-const apiUrl = 'http://localhost:4001'
+const apiUrl = 'fis.johnnythai.dev';
+// const apiUrl = 'http://localhost:4001';
 
 
 export { apiUrl };
